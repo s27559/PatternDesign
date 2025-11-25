534bb25e3d6e4d201a502b3abc078d6a8438a3a0
s27559 <s27559@pjwstk.edu.pl>
2025-11-25 20:17:48 +0100

    finish Adapter

--- app/src/main/java/org/PatternDesign/Program.java
+++ app/src/main/java/org/PatternDesign/Program.java
@@ -1,5 +1,7 @@
 package org.PatternDesign;
 
+import org.PatternDesign.Adapter.EmployeeAdapter;
+import org.PatternDesign.Adapter.ITraget;
 import org.PatternDesign.Factory.DataAnalyticsEngine;
 import org.PatternDesign.Factory.DatabaseFactory;
 import org.PatternDesign.Factory.DatabaseType;
@@ -11,16 +13,34 @@ public class Program {
                         dbEngines();
                         Thread.sleep(2000);
                         System.out.println();
+
+                        employeesAdapterDemo();
+                        Thread.sleep(2000);
+                        System.out.println();
                 } catch (InterruptedException e) {
                         e.printStackTrace();
                 }
-       }
-        
-        public static void dbEngines() {
+        }
+
+        static void dbEngines() {
                 DataAnalyticsEngine sqlEngine = new DataAnalyticsEngine(DatabaseFactory.CreateDatabase(DatabaseType.SqlServer));
                 sqlEngine.ProcessData("Query to process data for SQL Server");
 
                 DataAnalyticsEngine oracleEngine = new DataAnalyticsEngine(DatabaseFactory.CreateDatabase(DatabaseType.Oracle));
                 oracleEngine.ProcessData("Query to process data for Oracle");
+        }
+
+        static void employeesAdapterDemo() {
+                String[][] employeesArray = {
+                        {"101","John","SE","10000"},
+                        {"102","Smith","SE","20000"},
+                        {"103","Dev","SSE","30000"},
+                        {"104","Pam","SE","40000"},
+                        {"105","Sara","SSE","50000"}
+                };
+
+                System.out.println("HR system passes employee string array to Adapter\n");
+                ITraget traget = new EmployeeAdapter();
+                traget.ProcessCompanySalary(employeesArray);
         }
 }
