892c72a80f91e5243c39804bde0499f927433a8b
s27559 <s27559@pjwstk.edu.pl>
2025-11-25 19:39:29 +0100

    finish Factory

--- app/src/main/java/org/PatternDesign/Program.java
+++ app/src/main/java/org/PatternDesign/Program.java
@@ -1,6 +1,26 @@
 package org.PatternDesign;
 
+import org.PatternDesign.Factory.DataAnalyticsEngine;
+import org.PatternDesign.Factory.DatabaseFactory;
+import org.PatternDesign.Factory.DatabaseType;
+
 public class Program {
 
+       public static void main(String[] args) {
+                try {
+                        dbEngines();
+                        Thread.sleep(2000);
+                        System.out.println();
+                } catch (InterruptedException e) {
+                        e.printStackTrace();
+                }
+       }
         
+        public static void dbEngines() {
+                DataAnalyticsEngine sqlEngine = new DataAnalyticsEngine(DatabaseFactory.CreateDatabase(DatabaseType.SqlServer));
+                sqlEngine.ProcessData("Query to process data for SQL Server");
+
+                DataAnalyticsEngine oracleEngine = new DataAnalyticsEngine(DatabaseFactory.CreateDatabase(DatabaseType.Oracle));
+                oracleEngine.ProcessData("Query to process data for Oracle");
+        }
 }
